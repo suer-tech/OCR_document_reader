794ece60ba616d38cc959f44e6c99a33ffc1619d
suer-tech <suer2505@gmail.com>
2026-09-15 16:06:42 +0500

    Fix gateway sysctl setup and test runtime isolation

--- OCR-document-parser/admin_ops/vpn/healthcheck.sh
+++ OCR-document-parser/admin_ops/vpn/healthcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
+[ "$(sysctl -n net.ipv4.conf.all.src_valid_mark)" = 1 ]
 ip -4 link show dev awg0 >/dev/null
 nft list chain inet awg_lock output | grep -q 'policy drop'
 ip -4 route get 1.1.1.1 | grep -q ' dev awg0 '
